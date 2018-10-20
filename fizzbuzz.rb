afb034b930b921ea27eacca917321c5a5b3cf66f
shadowfox3735 <normanpan180@gmail.com>
2018-10-20 20:25:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,7 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
-  else if int % 5 == 0
-    "Buzz"
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
   end
 end
